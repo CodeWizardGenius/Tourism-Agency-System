c37bd33f54051b4ef22c027c5cf6806d2fbad260
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-11-09 01:16:38 +0300

    User Page Added

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -1,5 +1,67 @@
 package Model;
 
+import Helper.DBConnector;
+
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Admin extends User {
 
+    public boolean addUser(String username, String password, String type, String name) {
+        String query = "INSERT INTO tourism_agency_user (username, password, type, name) VALUES ('" + username + "', '" + password + "', '" + type + "', '" + name + "')";
+        try {
+            Statement statement = DBConnector.getConnection().createStatement();
+            statement.executeUpdate(query);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public ArrayList<User> getUserList() {
+        ArrayList<User> userList = new ArrayList<>();
+        User obj;
+        try {
+            Statement statement = DBConnector.getConnection().createStatement();
+            String query = "SELECT * FROM tourism_agency_user";
+            var resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                obj = new Admin();
+                obj.setId(resultSet.getInt("id"));
+                obj.setUsername(resultSet.getString("username"));
+                obj.setPassword(resultSet.getString("password"));
+                obj.setType(resultSet.getString("type"));
+                obj.setName(resultSet.getString("name"));
+                userList.add(obj);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return userList;
+    }
+
+    public boolean updateUser(String text, String text1, String string, String text2) {
+        String query = "UPDATE tourism_agency_user  SET  password = '" + text1 + "', type = '" + string + "', name = '" + text2 + "' WHERE username = '" + text + "'";
+        try {
+            Statement statement = DBConnector.getConnection().createStatement();
+            statement.executeUpdate(query);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteUser(String text) {
+        String query = "DELETE FROM tourism_agency_user WHERE username = '" + text + "'";
+        try {
+            Statement statement = DBConnector.getConnection().createStatement();
+            statement.executeUpdate(query);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
--- src/View/AdminGUI.java
+++ src/View/AdminGUI.java
@@ -1,17 +1,34 @@
 package View;
 
+import Helper.Helper;
 import Model.Admin;
+import Model.User;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class AdminGUI extends JFrame {
 
     private JPanel wrapper;
     private JButton btn_exit;
     private JLabel lbl_title;
+    private JTextField fld_username;
+    private JTextField fld_password;
+    private JComboBox cmb_user_type;
+    private JTextField fld_name_surname;
+    private JButton btn_user_add;
+    private JTable tbl_user_list;
+    private JTextField fld_delete_username;
+    private JButton btn_user_delete;
     private final Admin admin;
+    private DefaultTableModel model_user_list;
+    private Object[] row_user_list;
+
 
     public AdminGUI(Admin admin) {
         this.admin = admin;
@@ -27,6 +44,103 @@ public class AdminGUI extends JFrame {
             dispose();
             new LoginGUI();
         });
+
+        loadComboBoxUserList();
+        model_user_list = new DefaultTableModel();
+        Object[] col_user_list = {"Username", "Password", "Name Surname", "User Type"};
+        model_user_list.setColumnIdentifiers(col_user_list);
+        row_user_list = new Object[col_user_list.length];
+        tbl_user_list.setModel(model_user_list);
+        loadTableUserList();
+
+        btn_user_add.addActionListener(e -> {
+            if (fld_username.getText().isEmpty() || fld_password.getText().isEmpty() || fld_name_surname.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Please fill in the blanks.");
+            } else {
+                if (admin.addUser(fld_username.getText(), fld_password.getText(), cmb_user_type.getSelectedItem().toString(), fld_name_surname.getText())) {
+                    JOptionPane.showMessageDialog(null, "User added.");
+                    loadTableUserList();
+                } else {
+                    JOptionPane.showMessageDialog(null, "User could not be added.");
+                }
+                fld_username.setText("");
+                fld_password.setText("");
+                fld_name_surname.setText("");
+            }
+        });
+        tbl_user_list.getSelectionModel().addListSelectionListener(e -> {
+            int selectedRow = tbl_user_list.getSelectedRow();
+            if (selectedRow != -1) {
+                fld_delete_username.setText(tbl_user_list.getValueAt(selectedRow, 0).toString());
+            }
+        });
+        tbl_user_list.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    tbl_user_list_update();
+                }
+            }
+        });
+        btn_user_delete.addActionListener(e -> {
+            if (fld_delete_username.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Please fill in the blanks.");
+            } else {
+                if(Helper.confirm("Are you sure?", "Kullanici Sil", JOptionPane.YES_NO_OPTION)) {
+                    if (admin.deleteUser(fld_delete_username.getText())) {
+                        JOptionPane.showMessageDialog(null, "User deleted.");
+                        loadTableUserList();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "User could not be deleted.");
+                    }
+                }
+
+                fld_delete_username.setText("");
+            }
+        });
+    }
+
+    private void tbl_user_list_update() {
+        int selectedRow = tbl_user_list.getSelectedRow();
+        if (selectedRow != -1) {
+            String username = tbl_user_list.getValueAt(selectedRow, 0).toString();
+            String password = tbl_user_list.getValueAt(selectedRow, 1).toString();
+            String name = tbl_user_list.getValueAt(selectedRow, 2).toString();
+            String type = tbl_user_list.getValueAt(selectedRow, 3).toString();
+
+            if (admin.updateUser(username, password, type, name)) {
+                JOptionPane.showMessageDialog(null, "User updated.");
+                loadTableUserList();
+            } else {
+                JOptionPane.showMessageDialog(null, "User could not be updated.");
+            }
+        } else {
+            // Handle the case where no row is selected
+            JOptionPane.showMessageDialog(null, "Please select a user from the table to update.");
+        }
+    }
+
+
+    private void loadTableUserList() {
+        DefaultTableModel model_user_clear = (DefaultTableModel) tbl_user_list.getModel();
+        model_user_clear.setRowCount(0);
+        ArrayList<User> userList = admin.getUserList();
+        for (User user : userList) {
+            row_user_list[0] = user.getUsername();
+            row_user_list[1] = user.getPassword();
+            row_user_list[2] = user.getName();
+            row_user_list[3] = user.getType();
+            model_user_list.addRow(row_user_list);
+        }
+
+    }
+
+    private void loadComboBoxUserList() {
+        cmb_user_type.removeAllItems();
+        String[] userTypes = {"admin", "employee"};
+        for (String userType : userTypes) {
+            cmb_user_type.addItem(userType);
+        }
     }
 
 }
