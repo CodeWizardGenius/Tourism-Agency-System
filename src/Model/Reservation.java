6c6096d9a9caa168de65f775c1ff648b1d323dff
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-11-11 04:29:48 +0300

    reservation page

--- src/Model/Reservation.java
+++ src/Model/Reservation.java
@@ -1,4 +1,99 @@
 package Model;
 
+import Helper.DBConnector;
+
+import java.sql.Statement;
+
 public class Reservation {
+    private int id;
+    private int roomId;
+    private int employeeId;
+    private String startDate;
+    private String endDate;
+    public static boolean delete(int reservationId) {
+        String query = "DELETE FROM reservations WHERE id = " + reservationId;
+        try {
+            Statement statement= DBConnector.getConnection().createStatement();
+            statement.executeUpdate(query);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static Reservation getFetch(int reservationId) {
+        Reservation reservation = null;
+        String query = "SELECT * FROM reservations WHERE id = " + reservationId;
+        try {
+            Statement statement= DBConnector.getConnection().createStatement();
+            statement.executeQuery(query);
+            return reservation;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static boolean add(Reservation reservation) {
+        String query = "INSERT INTO reservations (room_id, employee_id, start_date, end_date) VALUES (" +
+                reservation.getRoomId() + ", " +
+                reservation.getEmployeeId() + ", " +
+                reservation.getStartDate() + ", " +
+                reservation.getEndDate() + ")";
+        try {
+            Statement statement= DBConnector.getConnection().createStatement();
+            statement.executeUpdate(query);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Reservation(int id, int roomId, int employeeId, String startDate, String endDate) {
+        this.id = id;
+        this.roomId = roomId;
+        this.employeeId = employeeId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
--- src/View/ReservationGUI.java
+++ src/View/ReservationGUI.java
@@ -1,18 +1,46 @@
 package View;
 
+import Helper.Helper;
 import Model.Employee;
+import Model.Reservation;
 import Model.Room;
 
 import javax.swing.*;
 
-public class ReservationGUI {
+public class ReservationGUI extends JFrame{
     private JPanel wrapper;
-    private JTable table1;
+    private JTable tbl_reservation_list;
     private JTextField fld_reservation_id;
     private JButton btn_reservation_delete;
+    private JTextField fld_room_no;
+    private JTextField fld_user_name;
+    private JButton btn_reservation_add;
+    private JTextField fld_start_date;
+    private JTextField fld_finish_date;
 
     public ReservationGUI(Room room, Employee employee) {
         wrapper = new JPanel();
         wrapper.add(new JLabel("Reservation for " + room.getName() + " by " + employee.getName()));
+        setSize(600, 600);
+        setTitle("Rezervasyon Ekrani");
+        setLocationRelativeTo(null); //ekranda ortada açılması için
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        btn_reservation_delete.addActionListener(e -> {
+            if (fld_reservation_id.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Rezervasyon ID bos olamaz!");
+            } else {
+                int reservation_id = Integer.parseInt(fld_reservation_id.getText());
+                if(Helper.confirm("Rezervasyonu silmek istediginizden emin misiniz?", "UYARI",2)) {
+                    if (Reservation.delete(reservation_id)) {
+                        JOptionPane.showMessageDialog(null, "Rezervasyon silindi!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Rezervasyon silinemedi!");
+                    }
+                }
+            }
+
+        });
     }
 }
